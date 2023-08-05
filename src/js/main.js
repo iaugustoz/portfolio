edb508b68842f0171e667b224b87f29b4a5fc3a2
Igor Augusto <igoraugustobz@gmail.com>
2023-08-05 10:43:45 -0300

    Cards criados

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+// Obtendo as referências para os elementos do DOM
 const menu = document.getElementById('menu');
 const slide = document.getElementById('animation');
 const listMenu = document.getElementById('list-menu');
@@ -6,6 +7,7 @@ let menuVisible = false; // Variável para controlar o estado do menu
 
 // Lógica que ativa a lista suspensa do menu
 menu.addEventListener('click', function() {
+  // Definindo as animações para o menu deslizante
   const slideIn = [
     {transform: 'translateX(100%)', easing: 'ease-out'},
     {transform: 'translateX(0%)', easing: 'ease-out'}
@@ -34,9 +36,10 @@ menu.addEventListener('click', function() {
   menuVisible = !menuVisible; // Inverte o estado do menu para a próxima interação
 });
 
+// Lógica para rolar para uma seção específica na página quando o botão de seta é clicado
 const arrow = document.getElementById('arrow');
 const destinationElement = document.getElementById('formacao');
 
 arrow.addEventListener('click', function() {
-    destinationElement.scrollIntoView({behavior: 'smooth'});
+  destinationElement.scrollIntoView({behavior: 'smooth'});
 });
