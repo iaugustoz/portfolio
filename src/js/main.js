7e10d61fc835eed98024287c33610dad0f19c106
Igor Augusto <igoraugustobz@gmail.com>
2023-08-20 14:37:48 -0300

    feat: adiciona seção de contato e recurso JavaScript

--- src/js/main.js
+++ src/js/main.js
@@ -43,3 +43,45 @@ const destinationElement = document.getElementById('formacoes');
 arrow.addEventListener('click', function() {
   destinationElement.scrollIntoView({behavior: 'smooth'});
 });
+
+
+// Obtendo as referências para o elemento do DOM
+const form = document.getElementById('form');
+
+// Adiciona um evento de escuta ao formulário para quando for submetido
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); // Impede o envio automático do formulário
+
+  // Obtém as referências para os elementos do formulário
+  const inputNome = document.getElementById('name');
+  const inputEmail = document.getElementById('email');
+  const inputMensagem = document.getElementById('message');
+
+  if ( (!inputNome.value.trim()) && (!inputEmail.value.trim()) && (inputMensagem.value.trim()) ) {
+
+    // Campos não preenchidos
+
+  } else {
+    // Campos preenchidos 
+
+    // Define um atraso de 2 segundos antes de exibir uma mensagem de sucesso usando o Swal (SweetAlert)
+    setTimeout(function() {
+
+      Swal.fire({
+        icon: 'success',
+        title: 'Tudo certo por aqui!',
+        text: 'Sucesso! Irei responder em breve.',
+        footer: '<a href="#inicio" id="fecharPopup">Voltar à página inicial?</a>'
+      });
+    
+      // Obtém a referência ao link de rodapé
+      const linkRodape = document.getElementById('fecharPopup');
+    
+      // Adiciona um evento de clique ao link de rodapé
+      linkRodape.addEventListener('click', function() {
+        Swal.close(); // Fecha o pop-up do Sweet Alert
+      });
+
+    }, 800);
+  }
+});
