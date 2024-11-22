a5891a146a52c85552aba6150b7684e370a87ffd
Igor Augusto <igoraugustobz@gmail.com>
2024-11-21 21:23:03 -0300

    :test_tube: test: validações padrões alteradas

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'portfolio-b' title`, () => {
+  it(`should have the 'portfolio' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('portfolio-b');
+    expect(app.title).toEqual('portfolio');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, portfolio-b');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, portfolio');
   });
 });
