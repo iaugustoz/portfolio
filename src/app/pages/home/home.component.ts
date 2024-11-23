a69168d30ddaf5d813962eef94dde899c096da33
Igor Augusto <igoraugustobz@gmail.com>
2024-11-22 21:01:30 -0300

    :lipstick: feat: HeaderComponent adicionado e estilizado

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../components/layout/header/header.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
