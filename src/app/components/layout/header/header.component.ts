656552b4d56873ed916abd5d6e71a16fc653cf86
Igor Augusto <igoraugustobz@gmail.com>
2024-11-22 22:32:22 -0300

    :recycle: refactor: arquivos CSS adicionados novamente

--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from '../../menu/menu.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MenuComponent],
   templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../../components/layout/header/header.component";
+import { HeaderComponent } from '../../components/layout/header/header.component';
+import { MenuComponent } from '../../components/menu/menu.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, MenuComponent],
   templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
