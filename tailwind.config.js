a2060c1d2f5e45e99ad2269b3ec3d560ec47d7af
Igor Augusto <igoraugustobz@gmail.com>
2024-11-30 10:27:58 -0300

    :recycle: refactor: logo adicionada ao projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
+        "playfair-display": ["playfair-display"], 
         "identification-05": ["identification-05"],
       },
     },
