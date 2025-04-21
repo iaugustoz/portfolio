967dcf6e393813015434c088f91473138c3156ff
Igor Augusto <igoraugustobz@gmail.com>
2025-04-21 16:00:33 -0300

    :sparkles: feat: adicionar seção de projetos

---
+++ src/app/features/portfolio/components/projects-section/projects-section.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { BadgeComponent } from '../../../../shared/components/badge/badge.component';
+
+@Component({
+  selector: 'app-projects-section',
+  standalone: true,
+  imports: [BadgeComponent],
+  templateUrl: './projects-section.component.html',
+  styleUrl: './projects-section.component.css',
+})
+export class ProjectsSectionComponent {
+  public technologies: string[] = ['Java', 'Spring Boot', 'Angular'];
+}
