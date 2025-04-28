2886f219acf819b3f22514ec8379581a3ca2975d
Igor Augusto <igoraugustobz@gmail.com>
2025-04-27 21:03:18 -0300

    :recycle: refactor: componentizar código

--- src/app/core/layouts/contact-section/contact-section.component.ts
+++ src/app/core/layouts/contact-section/contact-section.component.ts
@@ -1,13 +1,12 @@
 import { Component, Input } from '@angular/core';
-import socialMediaJson from '../../../../assets/data/card-social-media.json';
+import { SocialMediaComponent } from '../../../features/home/components/social-media/social-media.component';
+import { ContactFormComponent } from "../../../features/home/components/contact-form/contact-form.component";
 
 @Component({
   selector: 'app-contact-section',
   standalone: true,
-  imports: [],
+  imports: [SocialMediaComponent, ContactFormComponent],
   templateUrl: './contact-section.component.html',
   styleUrl: './contact-section.component.css',
 })
-export class ContactSectionComponent {
-  public json: any[] = socialMediaJson['social-media'];
-}
+export class ContactSectionComponent {}
