667c1781f28c858fc65d5a1c1b00d219af39eaf8
Igor Augusto <igoraugustobz@gmail.com>
2025-05-01 23:46:46 -0300

    :lipstick: feat: adicionar animações

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(),
-    provideAnimations(),
+    provideAnimationsAsync(),
   ],
 };
